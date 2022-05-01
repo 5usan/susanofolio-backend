cb31ea1cf7b823af7e07bf6e842e541a501588d6
5usan <susanshrestha2056@gmail.com>
2022-05-01 16:50:47 +0545

    mailing functionality implemented through gmail using oauth2

--- src/utils/mailHandler.js
+++ src/utils/mailHandler.js
@@ -1,8 +1,17 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
-const mailHandler = (email, description) => {
+const CLIENT_ID = "636310470691-lrtfpj44sfhk9t19mq10rro2s02omicg.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRET_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//04wFzZo2cJkR_CgYIARAAGAQSNwF-L9Ir6XGZtyABe6cXFnXtFkNC18fHHrDlsw-p9wVopMYwc6R0oiWr_e-ziI2pwRVQ6Xvvhkc";
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRET_URI);
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+const mailHandler =async (email, description) => {
   const contactMailOptions = {
-    from: "No Reply <no-reply@gmail.com>",
+    from: "Susan Shrestha <susanshrestha2056@gmail.com>",
     to: email,
     subject: "Appreciation Mail",
     text: "Thank you for reaching to me. I got your messgae and will reach you out soon.",
@@ -10,20 +19,24 @@ const mailHandler = (email, description) => {
   };
 
   const adminMailOptions = {
-    from: "No Reply <no-reply@gmail.com>",
-    to: "susanshrestha2056@gmail.com",
+    from: "Susan Shrestha <susanshrestha2056@gmail.com>",
+    to: "susanstha2056@gmail.com",
     subject: "Message through contact",
     text: description,
     html: `<p>${description}</p>`,
   };
 
   try {
+    const accessToken = await oAuth2Client.getAccessToken();
     const transporter = nodemailer.createTransport({
-      host: "smtp.mailtrap.io",
-      port: 2525,
+      service: "gmail",
       auth: {
-        user: "de23cb919ef3ea",
-        pass: "5795f857e31c26",
+        type: "oauth2",
+        user: "susanshrestha2056@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken
       },
     });
 
